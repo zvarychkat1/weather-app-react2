ca7e01ae160a773763c8cbf4de7a9bae4720671a
zvarychkat1 <neridova13@gmail.com>
2020-04-04 18:42:23 +0300

    api connected

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const api = {
+  key:"f3a6c97340407d168682f681c1093154",
+  base: "api.openweathermap.org/data/2.5/"
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
