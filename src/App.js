6a509337c50d733f0bd53efff52ebaf1c1ead877
zvarychkat1 <neridova13@gmail.com>
2020-04-05 20:06:08 +0300

    search-bar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 const api = {
   key:"f3a6c97340407d168682f681c1093154",
   base: "api.openweathermap.org/data/2.5/"
-
 }
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
       
+      </main>
     </div>
   );
 }
