2cfd8ef02c9d63c3d8db032945d01905f1ca8c66
zvarychkat1 <neridova13@gmail.com>
2020-04-06 21:19:26 +0300

    dateBuilder added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"];
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -17,7 +32,10 @@ function App() {
             placeholder="Search..."
           />
         </div>
-      
+        <div className="location-box">
+          <div className="location"></div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
       </main>
     </div>
   );
